188eaf4c5a7a30e107b00a7fe470b77cd2e7f8ee
Daniel King <danking@ccs.neu.edu>
2013-02-09 18:48:12 -0500

    Add a test-runner for all tests in the project
    The project needs a file which collects all the different tests into one
    file. The implementation of this is quite simple using rackunit. Each
    group of tests is placed inside a `test-suite' which is `provide'd to
    the test-runner. The test-runner coalesces these `test-suite's into a
    larger test suite which it then runs.
    
    The test-runner file contains a detailed description of how to add more
    tests.
    
    To execute the test-runner:
    
      racket run-all-tests.rkt

---
+++ tests/run-all-tests.rkt
@@ -0,0 +1,23 @@
+#lang racket
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; run-all-tests.rkt
+;;
+;; To add a new test-suite,
+;;
+;;  - add a file which `provide's a test suite containing various `test-cases'
+;;    or `check' forms.
+;;
+;;  - require the new test file in this file
+;;
+;;  - add the `provide'd test-suite to the test-suite in the run-tests form
+
+(require rackunit
+         rackunit/text-ui ;; for run-tests
+         "parse-command-line.rkt")
+
+(run-tests (test-suite "all-tests"
+                       parse-command-line-tests))
+
+
+
