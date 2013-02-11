40759cef1e9b3c46cfa0201862958d91b02a6f44
mmurray <mmurray@ioninteractive.com>
2013-02-10 20:38:15 -0500

    Printing for matches

---
+++ print-matches.rkt
@@ -0,0 +1,28 @@
+#lang racket
+
+(struct match
+  ( pattern-img
+    source-img
+    m1
+    n1
+    x
+    y))
+
+(define (print-match result)
+  (print (string-append
+          (match-pattern-img result)
+          " matches "
+          (match-source-img result)
+          " at "
+          (number->string (match-m1 result))
+          "x"
+          (number->string (match-n1 result))
+          "+"
+          (number->string (match-x result))
+          "+"
+          (number->string (match-y result))))
+  (newline))
+
+(define (print-matches results)
+  ;; filter matches for duplicates...
+  (for-each print-match results))
