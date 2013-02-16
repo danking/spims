eb9e843235ebbfc62a135c1813619aa82b8d37d3
Daniel King <danking@ccs.neu.edu>
2013-02-16 14:01:00 -0500

    Add main.rkt, the entry point to the project

---
+++ main.rkt
@@ -0,0 +1,11 @@
+#lang racket
+
+(require "Algorithm.rkt"
+         "load-image-file.rkt"
+         "parse-command-line.rkt")
+
+(let-values (((pattern-filename source-filename)
+              (parse-arguments (current-command-line-arguments))))
+  (let ((pattern-image (load-image-file pattern-filename))
+        (source-image (load-image-file source-filename)))
+    (match pattern-image source-image)))
