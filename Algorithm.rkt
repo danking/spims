9211bb7de44cb9839a02579907b6ee7a24faa959
David Glidden <dtglidden@gmail.com>
2013-02-18 00:06:00 -0500

    fixing a bug in the pattern matching algorithm
    Some of the boundary conditions, when checking if we reached the
    last pixel in the image, only used the '=' operator, and not the
    '>=' operator. One of the conditions causes a jump in the pixels
    that can skip the boundary condition, resulting in an infinite loop.

--- Algorithm.rkt
+++ Algorithm.rkt
@@ -14,7 +14,7 @@
 ;; Match takes a pattern (p) and target (t) vector of vectors of pixels and returns true if p exists in t
 (define (match p t p-filename t-filename)
   ;; Tolerance for JPEGs
-  (define tol 200)
+  (define tol 50)
   ;; Some useful information from our images
   (define p-rows (vector-length p))
   (define p-columns (vector-length (vector-ref p 0)))
@@ -32,7 +32,7 @@
   (if (and (valid-pixel? px py tx ty p-rows p-columns t-rows t-columns) (pixels-match-with-tolerance? p t px py tx ty tol))
       (cond
         ;;Pattern has been entirely checked, match confirmed!
-        [(and (= px (- p-columns 1)) (= py (- p-rows 1))) 
+        [(and (>= px (- p-columns 1)) (>= py (- p-rows 1)))
          (print-match (match-out (get-path-filename p-filename) (get-path-filename t-filename) p-columns p-rows (- tx px) (- ty py)))]
         ;;Pattern has reached the end of the line but continues on the next line
         [(and (= px (- p-columns 1)) (< py (- p-rows 1)))
@@ -41,20 +41,20 @@
         [else (match-pattern p t (+ px 1) py (+ tx 1) ty p-rows p-columns t-rows t-columns tol p-filename t-filename)])
       (cond
         ;;Each pixel in target has been checked and do not return a match
-        [(and (= tx (- t-columns 1)) (= ty (- t-rows 1))) (newline)]
+        [(and (>= tx (- t-columns 1)) (>= ty (- t-rows 1))) (newline)]
         ;;Restart match-pattern at the beginning of the next row
-        [(= tx (- t-columns 1)) (match-pattern p t 0 0 0 (+ ty 1) p-rows p-columns t-rows t-columns tol p-filename t-filename)]
+        [(>= tx (- t-columns 1)) (match-pattern p t 0 0 0 (+ ty 1) p-rows p-columns t-rows t-columns tol p-filename t-filename)]
         ;;Else move one right of the patterns upper left corner
         [else (match-pattern p t 0 0 (+ 1 (- tx px)) (- ty py) p-rows p-columns t-rows t-columns tol p-filename t-filename)])))
 
 
 ;; takes current pixel positions and image information and returns true if the pixel falls within the target.
 (define (valid-pixel? px py tx ty p-rows p-columns t-rows t-columns)
-  (and 
+  (and
    (<= ty (- t-rows 1))
    (<= tx (- t-columns 1))
    (<= py (- p-rows 1))
-   (<= px (- p-columns 1)) 
+   (<= px (- p-columns 1))
    (<= px tx)
    (<= py ty)
    (<= p-rows t-rows)
