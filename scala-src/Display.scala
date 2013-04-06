d65922c696f2c3f995b6a14ff69d6539d154fb68
David Glidden <dtglidden@gmail.com>
2013-04-06 15:05:22 -0400

    displaying image from scala REPL now works

--- scala-src/Display.scala
+++ scala-src/Display.scala
@@ -5,18 +5,23 @@ trait Display[-A] {
 object Display {
   import Bitmap._
   import java.awt.image.BufferedImage
-  import javax.swing.{JFrame, JPanel, JLabel}
+  import javax.swing.{JFrame, JPanel, JLabel, ImageIcon}
 
   def showImage(img: BufferedImage) {
+    val label = new JLabel(new ImageIcon(img))
     val panel = new JPanel
     val frame = new JFrame("Display")
+    panel.add(label)
     frame.add(panel)
+    /*frame.add(panel)
     val g = frame.getGraphics
     g.drawImage(img, 0, 0, panel)
-    g.dispose
+    g.dispose*/
     frame.pack
     frame.show
   }
+
+  def display[A](a: A)(implicit d: Display[A]) { d.display(a) }
 
   implicit object StringDisplay extends Display[String] {
     def display(s: String) = showImage(load(s))
@@ -24,5 +29,9 @@ object Display {
 
   implicit object BufferedImageDisplay extends Display[BufferedImage] {
     def display(bi: BufferedImage) = showImage(bi)
+  }
+
+  implicit object BitmapDisplay extends Display[Bitmap] {
+    def display(bmp: Bitmap) = showImage(bmp.toBufferedImage)
   }
 }
