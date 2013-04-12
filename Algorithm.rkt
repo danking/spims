2aedcefbf702742246d99cd056b9c224774cd6ad
Daniel King <danking@ccs.neu.edu>
2013-04-12 02:01:55 -0400

    Provide means for building alternative pattern-checkers

--- Algorithm.rkt
+++ Algorithm.rkt
@@ -16,6 +16,8 @@
   (find-pattern-in-source/tolerance pattern source 0))
 
 (define (find-pattern-in-source/tolerance pattern source pixel-difference-tolerance)
+  (define check-function apd/sc/plus-sign)
+
   (for*/list ((top-left-x (in-range 0 (add1 (- (bitmap-width source)
                                                (bitmap-width pattern)))))
               (top-left-y (in-range 0 (add1 (- (bitmap-height source)
@@ -25,9 +27,9 @@
               ;; and returns a sequence of length one, containing only the
               ;; given value
               (avg-diff (in-value
-                         (average-pixel-difference/short-circuiting top-left-x top-left-y
-                                                                    pattern source
-                                                                    pixel-difference-tolerance)))
+                         (check-function top-left-x top-left-y
+                                         pattern source
+                                         pixel-difference-tolerance)))
               ;; if the avg-diff is false, then don't execute the body
               #:when (begin (when (and (debug) (zero? (modulo top-left-x 300)))
                               (printf "  at (~a,~a), tolerance ~a, avg-diff ~a\n"
@@ -48,6 +50,47 @@
 (define (average-pixel-difference/short-circuiting top-left-x top-left-y
                                                    pattern source
                                                    average-difference-tolerance)
+  (apd/sc/pattern  top-left-x top-left-y
+                   pattern source
+                   average-difference-tolerance
+                   (in-parallel (in-range 0 (bitmap-width pattern))
+                                (in-range 0 (bitmap-height pattern)))))
+
+;; apd/sc/plus-sign : Natural Natural
+;;                    ImageBitmap ImageBitmap
+;;                    Number
+;;                    ->
+;;                    (U #f Number)
+;;
+(define (apd/sc/plus-sign top-left-x top-left-y
+                          pattern source
+                          average-difference-tolerance)
+  (let* ((height (bitmap-height pattern))
+         (width (bitmap-width pattern))
+         (y-midpt (floor (/ height 2)))
+         (x-midpt (floor (/ width 2))))
+    (apd/sc/pattern top-left-x top-left-y
+                    pattern source
+                    average-difference-tolerance
+                    (sequence-append
+                     ;; from 0 to just before midway, only check the row's middle value
+                     (in-parallel (in-value x-midpt) (in-range 0 y-midpt))
+                     ;; midway down, check the whole row
+                     (in-parallel (in-range 0 width) (in-value y-midpt))
+                     ;; after midway to the bottom, only check the row's middle value
+                     (in-parallel (in-value x-midpt) (in-range (add1 y-midpt) height))))))
+
+;; apd/sc/pattern : Natural Natural
+;;                  ImageBitmap ImageBitmap
+;;                  Number
+;;                  [Sequence Natural] [Sequence Natural]
+;;                  ->
+;;                  (U #f Number)
+;;
+(define (apd/sc/pattern top-left-x top-left-y
+                        pattern source
+                        average-difference-tolerance
+                        x/y-seq)
   (define (tolerable-average? average)
     (<= average average-difference-tolerance))
   (define pattern-size (* (bitmap-width pattern)
@@ -55,8 +98,7 @@
   (let ((difference
          (for*/fold
              ((total-difference 0))
-             ((current-x-of-pattern (in-range 0 (bitmap-width pattern)))
-              (current-y-of-pattern (in-range 0 (bitmap-height pattern)))
+             (((current-x-of-pattern current-y-of-pattern) x/y-seq)
               ;; if the total difference was set to #f in the last iteration, stop
               #:break (false? total-difference))
            (let* ((current-x-of-source (+ current-x-of-pattern top-left-x))
